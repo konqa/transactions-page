4a9d062808d10e045d817d073b784a5b92bd519b
Victor Bagu <victor.bagu@konqa.com>
2021-03-07 23:27:22 +0200

    add regex case insensitive search

--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -23,7 +23,7 @@ function Transactions(props: TransactionsProps) {
 
   useEffect(() => {
     console.log('props', props)
-  }, [])
+  }, [props])
 
   const resetSearch = (): void => {
     setTransactionRows(props.transactions)
@@ -34,14 +34,15 @@ function Transactions(props: TransactionsProps) {
   }
 
   const processSearch = (value: string) => {
-    let newRows = transactionRows.filter((transaction) =>
-      transaction.description.includes(value)
+    const regexp = new RegExp(value, 'gi')
+    let newRows = props.transactions.filter(
+      (transaction) => transaction.description.search(regexp) > -1
     )
-    console.log('newRows', newRows)
     setTransactionRows(newRows)
     setCurrentTotalTransactions(newRows.length)
     setCurrentPageNumber(1)
     setCurrentSearchText(value)
+    console.log('newRows', newRows)
   }
 
   const onSearch = (value: string) => {
@@ -56,6 +57,7 @@ function Transactions(props: TransactionsProps) {
   const onFlipPage = (value: number): void => {
     console.log(`new page ${value}`)
     setCurrentPageNumber(value)
+    document.querySelector('#transactions-block')?.scrollIntoView()
   }
 
   const handlePageSizeChange = (value: string): void => {
@@ -65,7 +67,7 @@ function Transactions(props: TransactionsProps) {
   }
 
   return (
-    <section className='transactions-block'>
+    <section className='transactions-block' id='transactions-block'>
       <div className='section-title'>Transactions</div>
       <div className='account-holder-filter-row'>
         <div className='section-title'>
@@ -73,7 +75,7 @@ function Transactions(props: TransactionsProps) {
             placeholder='Search'
             allowClear
             onSearch={onSearch}
-            style={{ width: 200 }}
+            style={{ width: 300 }}
           />
         </div>
         <div className='section-title'>
@@ -83,46 +85,53 @@ function Transactions(props: TransactionsProps) {
             style={{ width: 200 }}
             onChange={handlePageSizeChange}
           >
-            {pageSizes.map((size) => (
-              <Option value={size}>Show {size} entries</Option>
+            {pageSizes.map((size, index) => (
+              <Option key={index} value={size}>
+                Show {size} entries
+              </Option>
             ))}
           </Select>
         </div>
       </div>
-      <div className='details-block headings'>
-        <div>Date and Description</div>
-        <div>Category</div>
-        <div>Amount</div>
-      </div>
 
-      {transactionRows &&
-        transactionRows.map(
-          (transaction, index) =>
-            index > currentPageSize * (currentPageNumber - 1) &&
-            index < currentPageSize * currentPageNumber &&
-            transaction.description.includes(currentSearchText) && (
-              <div key={index} className='transaction-row bordered'>
-                <div className='transaction-row description'>
-                  {transaction.bookingDate.substring(0, 10)}
-                  <span className='transaction-bullet'>&bull;</span>
-                  {transaction.description}
-                </div>
-                <div className='transaction-row category'>
-                  {transaction.enrichedData.category.name ===
-                  'Uncategorised' ? (
-                    ''
-                  ) : (
-                    <div>{transaction.enrichedData.category.name}</div>
-                  )}
-                </div>
-                <div className='transaction-row amount'>{`${
-                  transaction.creditDebitIndicator === 'Debit' ? '-' : ''
-                }${transaction.amount.toFixed(2)}`}</div>
-              </div>
-            )
-        )}
+      {transactionRows.length > 0 ? (
+        <>
+          <div className='details-block headings'>
+            <div>Date and Description</div>
+            <div>Category</div>
+            <div>Amount</div>
+          </div>
 
-      {props.transactions && (
+          {transactionRows.map(
+            (transaction, index) =>
+              index >= currentPageSize * (currentPageNumber - 1) &&
+              index < currentPageSize * currentPageNumber && (
+                <div key={index} className='transaction-row bordered'>
+                  <div className='transaction-row description'>
+                    {transaction.bookingDate.substring(0, 10)}
+                    <span className='transaction-bullet'>&bull;</span>
+                    {transaction.description}
+                  </div>
+                  <div className='transaction-row category'>
+                    {transaction.enrichedData.category.name ===
+                    'Uncategorised' ? (
+                      ''
+                    ) : (
+                      <div>{transaction.enrichedData.category.name}</div>
+                    )}
+                  </div>
+                  <div className='transaction-row amount'>{`${
+                    transaction.creditDebitIndicator === 'Debit' ? '-' : ''
+                  }${transaction.amount.toFixed(2)}`}</div>
+                </div>
+              )
+          )}
+        </>
+      ) : (
+        <div className='no-transactions'>No transactions found</div>
+      )}
+
+      {transactionRows.length > 0 && (
         <div className='footer-page-flipper'>
           <Pagination
             defaultCurrent={1}
